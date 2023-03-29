a8986e2959f19fedc50bfc3c72b7472181fc4028
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2023-03-30 00:50:41 +0200

    burger menu heder redy + adoptive layout fix bugs

---
+++ Tomir-react-app/src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import { React, useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import "./index.scss";
+
+const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <>
+      <Navbar expand="md">
+        <Container className="mt-3">
+          <Navbar.Brand href="#">
+            <h1 className="header-nav__logo">
+              <span>T</span>OM<span>I</span>R
+            </h1>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            style={{
+              border: "none",
+              backgroundColor: "transparent",
+              boxShadow: "none",
+              outline: "none",
+            }}
+            onClick={() => setShowNav(!showNav)}
+          >
+            <span>
+              {showNav ? (
+                <FontAwesomeIcon icon={faBars} color="#000000" size="2x" />
+              ) : (
+                <FontAwesomeIcon icon={faTimes} color="#000000" size="2x" />
+              )}
+            </span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className="header-nav__menu-text" href="#">
+                Usługi
+              </Nav.Link>
+              <Nav.Link className="header-nav__menu-text" href="#">
+                Szkolenie
+              </Nav.Link>
+              <Nav.Link className="header-nav__menu-text" href="#">
+                Serwis
+              </Nav.Link>
+              <Nav.Link className="header-nav__menu-text" href="#">
+                Wynajem
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
