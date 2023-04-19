83bdcb04b153bcffd7a0f22148d8cbd423465f82
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2023-04-19 15:15:53 +0200

    add product Card

--- Tomir-react-app/src/components/Header/Header.jsx
+++ Tomir-react-app/src/components/Header/Header.jsx
@@ -11,74 +11,76 @@ const Header = () => {
 
   return (
     <>
-      <Navbar expand="md">
-        <Container className="mt-3">
-          <Navbar.Brand href="#">
-            <h1 className="header-nav__logo">
-              <span>T</span>OM<span>I</span>R
-            </h1>
-          </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            style={{
-              border: "none",
-              backgroundColor: "transparent",
-              boxShadow: "none",
-              outline: "none",
-            }}
-            onClick={() => setShowNav(!showNav)}
-          >
-            <span>
-              {showNav ? (
-                <FontAwesomeIcon icon={faTimes} color="#000000" size="2x" />
-              ) : (
-                <FontAwesomeIcon icon={faBars} color="#000000" size="2x" />
-              )}
-            </span>
-          </Navbar.Toggle>
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            style={{
-              border: "none",
-              zIndex: "10000",
-              backgroundColor: "#ffffff",
-              boxShadow: "none",
-              outline: "none",
-            }}
-          >
-            <Nav className="ms-auto">
-              <Nav.Link className="header-nav__menu-text" href="#">
-                <LinkElement
-                  LinkColor={"blackLinkColor"}
-                  LinkPath={"/"}
-                  LinkText={"O firme"}
-                />
-              </Nav.Link>
-              <Nav.Link className="header-nav__menu-text" href="#">
-                <LinkElement
-                  LinkColor={"blackLinkColor"}
-                  LinkPath={"/services"}
-                  LinkText={"Uslugi"}
-                />
-              </Nav.Link>
-              <Nav.Link className="header-nav__menu-text" href="#">
-                <LinkElement
-                  LinkColor={"blackLinkColor"}
-                  LinkPath={"/katalog"}
-                  LinkText={"Katalog"}
-                />
-              </Nav.Link>
-              <Nav.Link className="header-nav__menu-text" href="#">
-                <LinkElement
-                  LinkColor={"blackLinkColor"}
-                  LinkPath={"/contact"}
-                  LinkText={"Kontakt"}
-                />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <header>
+        <Navbar expand="md">
+          <Container className="mt-3">
+            <Navbar.Brand href="#">
+              <h1 className="header-nav__logo">
+                <span>T</span>OM<span>I</span>R
+              </h1>
+            </Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              style={{
+                border: "none",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+                outline: "none",
+              }}
+              onClick={() => setShowNav(!showNav)}
+            >
+              <span>
+                {showNav ? (
+                  <FontAwesomeIcon icon={faTimes} color="#000000" size="2x" />
+                ) : (
+                  <FontAwesomeIcon icon={faBars} color="#000000" size="2x" />
+                )}
+              </span>
+            </Navbar.Toggle>
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              style={{
+                border: "none",
+                zIndex: "10000",
+                backgroundColor: "#ffffff",
+                boxShadow: "none",
+                outline: "none",
+              }}
+            >
+              <Nav className="ms-auto">
+                <Nav.Link className="header-nav__menu-text" href="#">
+                  <LinkElement
+                    LinkColor={"blackLinkColor"}
+                    LinkPath={"/"}
+                    LinkText={"O firme"}
+                  />
+                </Nav.Link>
+                <Nav.Link className="header-nav__menu-text" href="#">
+                  <LinkElement
+                    LinkColor={"blackLinkColor"}
+                    LinkPath={"/services"}
+                    LinkText={"Uslugi"}
+                  />
+                </Nav.Link>
+                <Nav.Link className="header-nav__menu-text" href="#">
+                  <LinkElement
+                    LinkColor={"blackLinkColor"}
+                    LinkPath={"/katalog"}
+                    LinkText={"Katalog"}
+                  />
+                </Nav.Link>
+                <Nav.Link className="header-nav__menu-text" href="#">
+                  <LinkElement
+                    LinkColor={"blackLinkColor"}
+                    LinkPath={"/contact"}
+                    LinkText={"Kontakt"}
+                  />
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     </>
   );
 };
--- Tomir-react-app/src/pages/KatalogPage/KatalogPage.jsx
+++ Tomir-react-app/src/pages/KatalogPage/KatalogPage.jsx
@@ -1,11 +1,12 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import KatalogCard from "../../components/LayoutElements/KatalogCard/KatalogCard";
 
 const KatalogPage = () => {
   return (
     <>
       <Header />
-      <div style={{ color: "black", height: "100%", width: "100%" }}></div>
+      <KatalogCard />
       <Footer />
     </>
   );
